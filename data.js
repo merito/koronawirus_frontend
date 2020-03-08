eb7cc5baf567782b1feb7b1987139e390cb5d12c
Wolski <merito123@gmail.com>
2020-03-08 13:47:35 +0100

    Update 8.03.2020 13:47

--- data.js
+++ data.js
@@ -49,6 +49,30 @@ var points = [
     'last_modified_timestamp': '1583529325',
     'description': 'Pierwszy przypadek koronawirusa w Polsce',
     'source': ['https://www.gov.pl/web/koronawirus/pierwszy-przypadek-koronawirusa-w-polsce']
+  },{
+    'id': '21',
+    'name': 'Racibórz',
+    'location': {'lat': '50.091490', 'lon': '18.219813'},
+    'deaths': 0,
+    'cured': 0,
+    'infected': 1,
+    'type': 'ACTIVE',
+    'created_timestamp': '1583671445',
+    'last_modified_timestamp': '1583671445',
+    'description': '2 nowe przypadki zakażenia #koronawirus potwierdzono dziś u mężczyzny z woj. śląskiego i kobiety z woj. mazowieckiego. Pacjenci są hospitalizowani w szpitalach w Raciborzu i Warszawie. Oboje są w dobrym stanie. Służby sanitarno-epidemiologiczne docierają do wszystkich, którzy mogli mieć kontakt z zakażonymi. Część osób już trafiła na kwarantannę.',
+    'source': ['https://twitter.com/MZ_GOV_PL/status/1236628432676675585?ref_src=twsrc%5Etfw%7Ctwcamp%5Etweetembed%7Ctwterm%5E1236628432676675585&ref_url=https%3A%2F%2Fwiadomosci.gazeta.pl%2Fwiadomosci%2F7%2C114883%2C25768896%2Cministerstwo-zdrowia-sa-dwa-nowe-przypadki-koronawirusa-w.html']
+  },{
+    'id': '22,
+    'name': 'Warszawa',
+    'location': {'lat': '52.229832', 'lon': '21.011689'},
+    'deaths': 0,
+    'cured': 0,
+    'infected': 1,
+    'type': 'ACTIVE',
+    'created_timestamp': '1583671445',
+    'last_modified_timestamp': '1583671445',
+    'description': '2 nowe przypadki zakażenia #koronawirus potwierdzono dziś u mężczyzny z woj. śląskiego i kobiety z woj. mazowieckiego. Pacjenci są hospitalizowani w szpitalach w Raciborzu i Warszawie. Oboje są w dobrym stanie. Służby sanitarno-epidemiologiczne docierają do wszystkich, którzy mogli mieć kontakt z zakażonymi. Część osób już trafiła na kwarantannę.',
+    'source': ['https://twitter.com/MZ_GOV_PL/status/1236628432676675585?ref_src=twsrc%5Etfw%7Ctwcamp%5Etweetembed%7Ctwterm%5E1236628432676675585&ref_url=https%3A%2F%2Fwiadomosci.gazeta.pl%2Fwiadomosci%2F7%2C114883%2C25768896%2Cministerstwo-zdrowia-sa-dwa-nowe-przypadki-koronawirusa-w.html']
   }
 ];
 export {points};
