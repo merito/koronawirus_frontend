682ede9618f3a166e3a2071cb4f89d8b293ac0a8
Wolski <merito123@gmail.com>
2020-03-09 12:05:37 +0100

    Improve current marker location

--- src/components/Map.js
+++ src/components/Map.js
@@ -130,7 +130,7 @@ const Map = React.forwardRef(({
           icon={new Icon({
             iconUrl: '/location-icons/point.svg',
             iconSize: [50, 50],
-            iconAnchor: [25, 50],
+            iconAnchor: [23, 46],
           })}
           zIndexOffset={1000}
           position={activeMarker}
